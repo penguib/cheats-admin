71bfe7daf659e582a1ecc6e8325bb624f5bb84ee
penguib <42849406+penguib@users.noreply.github.com>
2020-10-29 11:51:33 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -590,15 +590,6 @@ Game.on("playerLeave", player => {
     }
 })
 
-Game.on("chat", (player, message) => {
-    if (!player.hex && !player.admin)
-        return Game.messageAll(`[#ffde0a]${player.username}\\c1:\\c0 ` + message)
-    if (!player.hex && player.admin)
-        return Game.messageAll(`[#ffde0a]${player.username}\\c1:\\c0 [#ffde0a]` + message)
-    
-    Game.messageAll(`[#ffde0a]${player.username}\\c1:[${player.hex}] ` + message)
-})
-
 const commands = {
     kill: (caller, args) => {
         getPlayersFromCommand(caller, args).forEach((victim) => {
